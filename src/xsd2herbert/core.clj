9a38001e48c6a4657fcee32419e064e24d697057
Jerome Prudent <jerome.prudent@vidal.fr>
2015-07-03 19:17:26 +0200

    done simpleType/restriction/string/enumeration

--- src/xsd2herbert/core.clj
+++ src/xsd2herbert/core.clj
@@ -1,6 +1,72 @@
-(ns xsd2herbert.core)
+(ns xsd2herbert.core
+  (:require [miner.herbert.predicates :refer [str?]]
+            [clojure.data.xml :refer [parse-str]])
+  (:import (clojure.data.xml Element)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn find-schemas
+  "returns a set of all schemas found in xml document"
+  [^Element parsed-xml]
+  (let [schemas (atom #{})]
+    (clojure.walk/prewalk
+      (fn [x]
+        (if (map? x)
+          (if (= :schema (:tag x))
+            (do (swap! schemas conj x)
+                nil)
+            (:content x))
+          x))
+      parsed-xml)
+    @schemas))
+
+(defn in?
+  "Herbert predicat that returns true if s is in xs"
+  [xs s] (xs s))
+
+(defmulti basetype->herbert
+          "convert a base type to an Herbert predicate"
+          (fn [base] base))
+
+(defmethod basetype->herbert "xsd:string" [_]
+  '(pred str?))
+
+(defn enumerations->herbert
+  "Convert a list of enumerations to an Herbert predicate"
+  [enumerations]
+  {:pre [(not-any? #(not= :enumeration (:tag %)) enumerations)]}
+  (let [values (->> (map #(get-in % [:attrs :value]) enumerations)
+                    (reduce conj #{}))]
+    (list 'pred 'in? values)))
+
+(defn handle-restriction
+  "Convert an XSD restriction of a type to a list of Herbert predicates"
+  [restriction]
+  {:pre (= :restriction (:tag restriction))}
+  (let [base (-> (get-in restriction [:attrs :base])
+                 basetype->herbert)
+        enumerations (->> (:content restriction)
+                          (filter (partial #(= :enumeration (:tag %))))
+                          enumerations->herbert)]
+    [base enumerations]))
+
+
+
+(defn define-simple-type
+  "take an XSD simpleType and returns an Herbert type definition"
+  [simple-type]
+  {:pre  [(= (:tag simple-type) :simpleType)]
+   :post [(= 'and (first (:herbert %)))]}
+  (let [type (get-in simple-type [:attrs :name])
+        herberts (->> (map handle-restriction (:content simple-type))
+                      (apply concat)
+                      (reduce conj (list 'and))
+                      reverse)]
+    {:type type, :herbert herberts}))
+
+(defn xms->herbert
+  "Take a string and returns a seq of Herbert type definition"
+  [^String xml]
+  (->> (parse-str xml)
+       (find-schemas)
+       (map :content)
+       (apply concat)
+       (map define-simple-type)))
--- test/xsd2herbert/core_test.clj
+++ test/xsd2herbert/core_test.clj
@@ -1,7 +1,54 @@
 (ns xsd2herbert.core-test
   (:require [clojure.test :refer :all]
-            [xsd2herbert.core :refer :all]))
+            [xsd2herbert.core :refer :all]
+            [miner.herbert.predicates :refer [str?]]
+            [clojure.data.xml :refer [parse-str]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def single-simple-type-xsd "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<xsd:schema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"
+            attributeFormDefault=\"qualified\"
+            elementFormDefault=\"qualified\"
+            targetNamespace=\"urn:Vidal\">
+    <xsd:simpleType name=\"DoseUnit\">
+        <xsd:restriction base=\"xsd:string\">
+            <xsd:enumeration value=\"MG\"/>
+            <xsd:enumeration value=\"G\"/>
+            <xsd:enumeration value=\"ML\"/>
+            <xsd:enumeration value=\"MCG\"/>
+            <xsd:enumeration value=\"U\"/>
+            <xsd:enumeration value=\"KU\"/>
+            <xsd:enumeration value=\"MU\"/>
+            <xsd:enumeration value=\"MMOL\"/>
+        </xsd:restriction>
+    </xsd:simpleType>
+</xsd:schema>")
+
+
+(deftest should-find-schema
+  (testing "should find all schemas but not nested ones"
+    (let [schemas (find-schemas {:tag     :foo
+                                 :content [{:tag     :schema
+                                            :content [{:tag :schema :name :nested}]
+                                            :name    :top}
+                                           {:tag     :bar
+                                            :content [{:tag :tennis}
+                                                      {:tag :schema :name :inner1}]}
+                                           {:tag :schema :name :inner2}]})
+          schema-names (set (map :name schemas))]
+      (is (= #{:top :inner1 :inner2} schema-names)))))
+
+
+(deftest should-define
+  (testing "a single simple type which is a restriction of string by enumeration"
+    (let [simple-type (define-simple-type (-> (parse-str single-simple-type-xsd)
+                                              (get-in [:content])
+                                              first))]
+      (is (= simple-type {:type    "DoseUnit"
+                          :herbert '(and (pred str?) (pred in? #{"MG" "G" "ML" "MCG" "U" "KU", "MU" "MMOL"}))})))))
+
+(deftest should-convert-xsd-to-herbert
+  (testing "simple type/restriction/string/enum"
+    (let [xml single-simple-type-xsd]
+      (is (= (xms->herbert xml)
+             [{:type    "DoseUnit"
+               :herbert '(and (pred str?) (pred in? #{"MG" "G" "ML" "MCG" "U" "KU", "MU" "MMOL"}))}])))))
